16946a59a8fc6875373d189019f2f6689585cfdf
Adhishthatri <asunasingh2003@gmail.com>
2025-07-18 00:31:34 +0530

    Auth functions adding

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,21 @@ import Add from './pages/Add'
 import List from './pages/List'
 import Orders from './pages/Orders'
 import Login from './components/Login'
+import { ToastContainer } from 'react-toastify';
+
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL
 
 const App = () => {
 
     const [token , setToken] = useState('');
 
 
-
-
   return (
     <div className='bg-gray-50 min-h-screen'>
+      <ToastContainer/>
         {token === "" ?
-         <Login/> 
+         <Login setToken={setToken}/> 
          :
               <>
               <Navbar />
